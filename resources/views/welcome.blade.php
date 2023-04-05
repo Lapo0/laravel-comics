a4bfc4b83219e72fa0ae63d23fa48ece6d6b33ab
Lapo0 <lapo.gini.01@gmail.com>
2023-04-05 17:23:06 +0200

    FEAT style in mnai

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,6 @@
 
     <div>
         <div class="container">
-            <h1>
-                MAIN
-            </h1>
-        </div>
-        <div class="container">
             @dump($comics)
 
             <div class="row">
