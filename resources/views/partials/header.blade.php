5d28cde450fd5750119a808f54566a3e7b104d4e
Lapo0 <lapo.gini.01@gmail.com>
2023-04-05 16:51:00 +0200

    FEAT main $comic && header style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,24 @@
 <header>
-    <div class="container">
-        <h1>
-            HEADER
-        </h1>
+<div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="logo">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                </div>
+            </div>
+            <div class="col">
+                <ul class="link">
+                    @foreach($links as $link)
+
+                        <li>
+                            <a href="#">
+                                {{ $link }}
+                            </a>
+                        </li>
+
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,19 @@
         </div>
         <div class="container">
             @dump($comics)
+
+            <div class="row">
+                @foreach($comics as $comic)
+
+                    <div class="col-2">
+                        <img src="{{ $comic['thumb'] }}" alt="">
+                        <h3>
+                            {{ $comic['title'] }}
+                        </h3>
+                    </div>
+
+                @endforeach
+            </div>
         </div>
         
     </div>
