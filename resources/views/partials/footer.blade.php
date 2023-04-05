f69a1fb7130d79d80c511279bc5c95415892f5e4
Lapo0 <lapo.gini.01@gmail.com>
2023-04-05 15:31:22 +0200

    FEAT layouts with header && footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <h1>
+            FOOTER
+        </h1>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="container">
+        <h1>
+            HEADER
+        </h1>
+    </div>
+</header>
